fc525b0c84e9eba82f87c16c47c7f1cf718377e5
mhavelant <mhavelant@brainsum.com>
2019-04-08 16:49:17 +0200

    EL-842 | Add loading for the page map.

--- src/Helper/PageResolverTrait.php
+++ src/Helper/PageResolverTrait.php
@@ -2,17 +2,64 @@
 
 namespace Brainsum\DrupalBehatTesting\Helper;
 
+use Behat\Behat\Tester\Exception\PendingException;
+use Symfony\Component\Yaml\Parser;
+
+/**
+ * Trait PageResolverTrait.
+ *
+ * @package Brainsum\DrupalBehatTesting\Helper
+ */
 trait PageResolverTrait {
 
-  protected $pageNameMapping;
+  /**
+   * Page mapping.
+   *
+   * Associative array for the page map.
+   *
+   * @var array
+   */
+  protected $pageMapping;
 
-  // @todo: Get these from config.
-  // /**
-  //  * Reads the config.yml file and populates the $config property.
-  //  */
-  // public function loadConfig() {
-  //   $this->config = (new \Symfony\Component\Yaml\Parser())->parse(file_get_contents(static::CONFIG_FILE));
-  // }
-  // path-mapping.yml
+  /**
+   * Load the page mapping config from file.
+   *
+   * @param string $filePath
+   *   File path.
+   */
+  protected function loadPageMapping(string $filePath): void {
+    $this->pageMapping = (new Parser())->parse($filePath);
+  }
+
+  /**
+   * Returns the page mapping.
+   *
+   * @return array
+   *   The page mapping.
+   */
+  protected function pageMapping(): array {
+    if (empty($this->pageMapping)) {
+      return [];
+    }
+
+    return $this->pageMapping;
+  }
+
+  /**
+   * Resolve a page.
+   *
+   * @param string $pageName
+   *   The page name.
+   *
+   * @return string
+   *   The page path.
+   */
+  protected function resolvePageNameToPath(string $pageName): string {
+    if (isset($this->pageMapping()['path'])) {
+      return $this->pageMapping()['path'];
+    }
+
+    throw new PendingException("Path for '$pageName' is not configured.");
+  }
 
 }
